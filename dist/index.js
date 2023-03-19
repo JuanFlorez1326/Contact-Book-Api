a3e765801cc0c11458cf3cf7135ec6eb0f5eaf0e
Juan Pablo Patiño <82609508+JuanFlorez1326@users.noreply.github.com>
2023-03-18 22:20:33 -0500

    ADD: Config database

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./database");
-const app_1 = require("./app");
-app_1.app.listen(app_1.app.get('port'), () => {
-    console.log('Contact Book server running on port', app_1.app.get('port'));
-});
